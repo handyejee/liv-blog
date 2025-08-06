2a809c072d2396c22a8e47c46389c31a5cfd8020
yjson <handyejee14@gmail.com>
2025-08-06 22:24:48 +0900

    fix: 파비콘 위치 _app.js 파일로 변경

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,14 @@ export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute='class' defaultTheme={siteMetadata.theme}>
       <Head>
-        <meta content='width=device-width, initial-scale=1' name='viewport' />
+        <link rel='icon' href='/static/favicons/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='76x76' href='/static/favicons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/static/favicons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/static/favicons/favicon-16x16.png' />
+        <link rel='manifest' href='/static/favicons/site.webmanifest' />
+        <link rel='mask-icon' href='/static/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
