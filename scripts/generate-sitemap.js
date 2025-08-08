5d9f513334facc46e160ec811b47f17408dbe52f
yjson <handyejee14@gmail.com>
2025-08-08 16:08:39 +0900

    Fix:  Fix sitemap generation script

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -15,46 +15,45 @@ const siteMetadata = require('../data/siteMetadata')
     '!pages/api',
   ])
 
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map((page) => {
-                // Exclude drafts from the sitemap
-                if (page.search('.md') >= 1 && fs.existsSync(page)) {
-                  const source = fs.readFileSync(page, 'utf8')
-                  const fm = matter(source)
-                  if (fm.data.draft) {
-                    return
-                  }
-                  if (fm.data.canonicalUrl) {
-                    return
-                  }
-                }
-                const path = page
-                  .replace('pages/', '/')
-                  .replace('data/blog', '/blog')
-                  .replace('public/', '/')
-                  .replace('.js', '')
-                  .replace('.tsx', '')
-                  .replace('.mdx', '')
-                  .replace('.md', '')
-                  .replace('/feed.xml', '')
-                const route = path === '/index' ? '' : path
+  const urls = pages
+    .map((page) => {
+      // Exclude drafts from the sitemap
+      if (page.search('.md') >= 1 && fs.existsSync(page)) {
+        const source = fs.readFileSync(page, 'utf8')
+        const fm = matter(source)
+        if (fm.data.draft) {
+          return null
+        }
+        if (fm.data.canonicalUrl) {
+          return null
+        }
+      }
+      const path = page
+        .replace('pages/', '/')
+        .replace('data/blog', '/blog')
+        .replace('public/', '/')
+        .replace('.js', '')
+        .replace('.tsx', '')
+        .replace('.mdx', '')
+        .replace('.md', '')
+        .replace('/feed.xml', '')
+      const route = path === '/index' ? '' : path
 
-                if (page.search('pages/404.') > -1 || page.search(`pages/blog/[...slug].`) > -1) {
-                  return
-                }
-                return `
-                        <url>
-                            <loc>${siteMetadata.siteUrl}${route}</loc>
-                        </url>
-                    `
-              })
-              .join('')}
-        </urlset>
-    `
+      if (page.search('pages/404.') > -1 || page.search(`pages/blog/[...slug].`) > -1) {
+        return null
+      }
+      return `  <url>
+    <loc>${siteMetadata.siteUrl}${route}</loc>
+  </url>`
+    })
+    .filter(Boolean) // null 값 제거
 
-  // eslint-disable-next-line no-sync
+  const sitemap = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    ...urls,
+    '</urlset>',
+  ].join('\n')
+
   fs.writeFileSync('public/sitemap.xml', sitemap)
 })()
