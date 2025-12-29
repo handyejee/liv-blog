99bcf6fe210d211daa3f9bc9cbfead68b52e6cfc
yjson <handyejee14@gmail.com>
2025-12-29 15:36:41 +0900

    fix: 블로그 메인에서 전체 글 목록을 조회할 수 있도록 수정

--- pages/blog.js
+++ pages/blog.js
@@ -1,31 +1,12 @@
-import { getAllFilesFrontMatter } from '@/lib/mdx'
-import siteMetadata from '@/data/siteMetadata'
-import ListLayout from '@/layouts/ListLayout'
-import { PageSEO } from '@/components/SEO'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-export const POSTS_PER_PAGE = 5
+export default function BlogRedirect() {
+  const router = useRouter()
 
-export async function getStaticProps() {
-  const posts = await getAllFilesFrontMatter('blog')
-  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
-  const pagination = {
-    currentPage: 1,
-    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
-  }
+  useEffect(() => {
+    router.replace('/')
+  }, [router])
 
-  return { props: { initialDisplayPosts, posts, pagination } }
-}
-
-export default function Blog({ posts, initialDisplayPosts, pagination }) {
-  return (
-    <>
-      <PageSEO title={`Blog - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <ListLayout
-        posts={posts}
-        initialDisplayPosts={initialDisplayPosts}
-        pagination={pagination}
-        title='All Posts'
-      />
-    </>
-  )
+  return null
 }
